932f2f087454aa8c5918cf8a4b7a908538f9d881
Kamal-nayan123 <kamalnayan41114@gmail.com>
2025-05-14 18:32:52 +0530

    sync changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,16 +6,16 @@ import { isAuthenticated } from "@/lib/actions/auth.actions";
 const Authlayout = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
 
-  useEffect(() => {
-    const checkAuth = async () => {
-      const isUserAuthenticated = await isAuthenticated();
-      if (isUserAuthenticated) {
-        router.push("/"); // Redirect to the home page
-      }
-    };
+  // useEffect(() => {
+  //   const checkAuth = async () => {
+  //     const isUserAuthenticated = await isAuthenticated();
+  //     if (isUserAuthenticated) {
+  //       router.push("/"); // Redirect to the home page
+  //     }
+  //   };
 
-    checkAuth();
-  }, [router]);
+  //   checkAuth();
+  // }, [router]);
 
   return <div className="auth-layout">{children}</div>;
 };
